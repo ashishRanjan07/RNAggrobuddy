9d52a80df88ffb487ebb8f01f9dd7398eb704674
ashishranjan9629 <ashish.ranjan@velocis.co.in>
2024-06-20 18:01:56 +0530

    check navigation container and change the navigation

--- src/redux/reducer/Reducer.js
+++ src/redux/reducer/Reducer.js
@@ -1,10 +1,11 @@
-import {CHANGE_LANGUAGE, LOGIN, THEME_CHANGE} from '../action/Action';
+import {CHANGE_LANGUAGE, LOGIN, SAVEDATA, THEME_CHANGE} from '../action/Action';
 
 //Initial State
 const initialState = {
   language: 'en',
   mode: 'light',
   isLoggedIn: 'No',
+  saveData:'null',
 };
 
 // Reducer
@@ -25,6 +26,11 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         isLoggedIn: action.payload,
       };
+      case SAVEDATA:
+        return {
+          ...state,
+          saveData: action.payload,
+        };
     default:
       return state;
   }
